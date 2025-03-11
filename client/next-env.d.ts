697cd496dc95ecf5ff099805593df2f2249dd6bd
Rafi Be <rafixks@interia.pl>
2025-03-11 03:21:45 +0100

    Poprawka deploya FTP na cPanel

--- client/next-env.d.ts
+++ client/next-env.d.ts
@@ -1,17 +1,4 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-// NOTE: This file should not be edited
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-// see https://nextjs.org/docs/pages/building-your-application/configuring/typescript for more information.
-=======
-// see https://nextjs.org/docs/basic-features/typescript for more information.
->>>>>>> 5185817 (feat: add new components and pages for timer, three.js integration, and welcome screen)
-=======
-// see https://nextjs.org/docs/basic-features/typescript for more information.
->>>>>>> 5185817 (feat: add new components and pages for timer, three.js integration, and welcome screen)
-=======
-// see https://nextjs.org/docs/basic-features/typescript for more information.
->>>>>>> 51858176c37d86a5a1b3637f910f85a822ad44a9
+
